8669934aca105704ad3c405d7379147622af742d
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-05 21:13:44 +0800

    initialize

---
+++ global_aid_network/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4c!kj*!j_r9a^4yo5*#_k7n2n_epsgz5ayhrdnhz*4nynfpge3"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
